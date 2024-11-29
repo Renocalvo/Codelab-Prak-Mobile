d82262a661887a452ba502377d239d9d31f307bf
Saya__IB <indrabayu210903@gmail.com>
2024-11-29 11:23:33 +0700

    feat: geolocator

--- codelab/lib/app/modules/baca_page/controllers/baca_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
-//import 'dart:io';
-import 'package:flutter/material.dart';
-
-class bacaController extends GetxController {
-  var pageTitle = "Lorem Ipsum".obs;
-}
--- codelab/lib/app/modules/login/controllers/login_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class LoginController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
-
-  void increment() => count.value++;
-}
